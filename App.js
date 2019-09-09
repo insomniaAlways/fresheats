ef85e7a922e2b8d57e490d718c2b58bd2aa24665
trideep.k <trideep.k@talview.com>
2019-09-10 00:04:23 +0530

    cart screen added WIP

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LandingScreen from './src/Landing';
+import Cart from './src/screens/Cart';
 
 export default function App() {
-  return <LandingScreen />
+  return <Cart />
 }
 
 const styles = StyleSheet.create({
---
+++ src/screens/Cart.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native';
+
+export default function App() {
+  let itemList = (
+    <View style={{marginBottom: 10}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <View style={{flex: 5}}>
+          <Text style={{fontSize: 16, fontWeight: '500'}}>Hello</Text>
+        </View>
+        <View style={{ 
+            flex: 2,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            borderColor: 'grey',
+            borderWidth: 1,
+          }}>
+          <View style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRightColor: 'grey',
+              borderRightWidth: 1,
+              width: 18,
+              paddingRight: 6
+            }}>
+            <Text style={{fontSize: 16}}>-</Text>
+          </View>
+          <Text style={{fontSize: 16}}>1</Text>
+          <View style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderLeftColor: 'grey',
+              borderLeftWidth: 1,
+              width: 18,
+              paddingLeft: 7
+            }}>
+            <Text>+</Text>
+          </View>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginLeft: 10}}>
+          <Text style={{fontSize: 16, textAlign: 'center'}}>$123</Text>
+        </View>
+      </View>
+    </View>
+  );
+  return (
+    <View style={styles.container}>
+      <View style={{flex:3, justifyContent: 'flex-start', padding: 20, paddingBottom: 0, backgroundColor: '#fff'}}>
+        <Text style={{fontSize: 20, fontWeight: '500', marginBottom: 10, borderBottomColor: '#eee', borderBottomWidth: 1}}>Added Items</Text>
+        {itemList}
+        {itemList}
+        {itemList}
+        {itemList}
+        {itemList}
+      </View>
+      <View style={{flex: 2,padding: 20, paddingBottom: 0, backgroundColor: '#fff', marginTop: 10}}>
+        <Text style={{fontSize: 20, fontWeight: '500'}}>Bill Details</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 10}}>
+          <Text style={{fontSize: 16}}>Item Total</Text>
+          <Text style={{fontSize: 16}}>$123</Text>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text style={{fontSize: 16}}>Delivery Charge</Text>
+          <Text style={{fontSize: 16}}>$123</Text>
+        </View>
+        <View style={{borderTopColor: '#eee', borderTopWidth: 1, marginTop: 10, marginBottom: 10}}></View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text style={{fontWeight: '500', fontSize: 16}}>To Pay</Text>
+          <Text style={{fontWeight: '500', fontSize: 16}}>$123</Text>
+        </View>
+      </View>
+      <View style={{flex: 3}}>
+
+      </View>
+      <View style={{flex:1, justifyContent: 'center', backgroundColor: '#fff', alignItems: 'center', backgroundColor: "#4CA3DA"}}>
+        <Text style={{fontWeight: '500', fontSize: 20, color: '#fff'}}>Select Address</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    flex:1,
+    justifyContent: 'space-between'
+  },
+  container: {
+    flex: 1,
+    marginTop:24,
+    backgroundColor: '#eee'
+    // padding: 20,
+  },
+  
+});
