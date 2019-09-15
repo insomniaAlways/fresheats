a2422599e70652bbd5d3ddb6bfd97ac746fbe4b8
trideep.k <trideep.k@talview.com>
2019-09-15 20:21:57 +0530

    app redirection changed to restaurant

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ItemList from './src/screens/Restaurants/ItemList';
+import RestaurantList from './src/screens/Restaurants/RestaurantList';
 
 export default function App() {
-  return <ItemList />
+  return <RestaurantList />
 }
 
 const styles = StyleSheet.create({
